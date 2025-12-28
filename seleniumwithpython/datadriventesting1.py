45878a076df739118c19d2e2b75236d0eb48fdca
Sandipa Rimal <rimal.sandipa018@gmail.com>
2025-12-28 11:01:10 +0545

    datadriventesting using csv

---
+++ seleniumwithpython/datadriventesting1.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import csv
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+csv_file='credential.csv'
+test_data=[]
+
+with open(csv_file,'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        test_data.append(row)
+print(test_data)
+for data in test_data:
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.saucedemo.com/")
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys(data['username'])
+    driver.find_element(By.ID, "password").send_keys(data['password'])
+    driver.find_element(By.ID, "login-button").click()
+    time.sleep(2)
+    driver.quit()
