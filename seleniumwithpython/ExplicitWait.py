739a0c1a552ea49ca7b2c55fc598b43b970c5046
Sandipa Rimal <rimal.sandipa018@gmail.com>
2025-12-29 09:41:36 +0545

    ExplicitWait

---
+++ seleniumwithpython/ExplicitWait.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.maximize_window()
+wait=WebDriverWait(driver, 10)
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+driver.find_element(By.ID, "react-burger-menu-btn").click()
+wait.until(EC.visibility_of_element_located((By.ID,"logout_sidebar_link"))).click()
+
+
+driver.quit()
